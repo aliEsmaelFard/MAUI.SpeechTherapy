cbd8fe282927a268f812e18efa62840ce2c38f2a
aliesmaelfard <aliesmaelfard@gmail.com>
2024-01-27 01:06:41 +0330

    Create project basees and add libs

---
+++ MAUI.SpeechTherapy/Models/TestModel.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAUI.SpeechTherapy.Models
+{
+    public class TestModel
+    {
+        [PrimaryKey , AutoIncrement]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Data { get; set; }
+    }
+}
